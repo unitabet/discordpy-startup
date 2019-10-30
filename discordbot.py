0ac689ba071ce4aa6d23f9e101fa8490c0ec33f0
unitabet <57191743+unitabet@users.noreply.github.com>
2019-10-31 02:51:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,14 +10,6 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
     
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == 'neko':
-        await message.channel.send('にゃーん')
 
 @bot.command()
 async def ping(ctx):
