478c5294495e53255299a17fe528f5615412a830
unitabet <57191743+unitabet@users.noreply.github.com>
2019-10-31 03:24:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,9 +15,9 @@ async def on_message(message):
     if message.author.bot:
         return
     # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
+    if message.content == 'neko':
         await message.channel.send('にゃーん')
-    if message.content == 'rand':
+    if message.content == '/rand':
         num = random.randint(1,10)
         await message.channel.send('num')
     # メンバーのリストを取得して表示
