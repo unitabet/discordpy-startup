7e4c2e877b133d43a8e6839a2f34d4e38edcd965
unitabet <57191743+unitabet@users.noreply.github.com>
2019-10-31 03:03:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,14 +4,6 @@ import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
         
 @bot.event
 async def on_command_error(ctx, error):
